a4d3f15189c5f50883e4f3f176fe674bbd2c6b88
ZippyDaFish <andrew.riker21@gmail.com>
2023-01-20 13:53:33 -0500

    Begin data extraction for five day forecast

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 async function getWeatherData(city, unit){
     const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q='+city+'&units='+unit+'&APPID=1bc93ee0ee0d4cdd8fd91c4abca090f2');
     const weatherData = await response.json();
-    console.log(weatherData);
     assignWeatherData(weatherData);
 }
 function assignWeatherData(weatherData){
@@ -15,16 +14,20 @@ function assignWeatherData(weatherData){
 async function getForecastData(city, unit){
     const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&units='+unit+'&APPID=1bc93ee0ee0d4cdd8fd91c4abca090f2');
     const forecastData = await response.json();
-    console.log(forecastData);
     assignForecastData(forecastData.list);
 }
 function assignForecastData(forecastDataList){
     let dataInDays = [];
-    dataInDays.push(forecastDataList[0])
+    let day = extractUsefulForecastData(forecastDataList[0]);
+    dataInDays.push(day);
     for(i = 5; i < forecastDataList.length; i += 8){
-        dataInDays.push(forecastDataList[i]);
+        day = extractUsefulForecastData(forecastDataList[i]);
+        dataInDays.push(day);
     }
     console.log(dataInDays);
+}
+function extractUsefulForecastData(day){
+    return day;
 }
 getWeatherData('Lansing', 'metric');
 getForecastData('Lansing', 'metric');
