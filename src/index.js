27b20797984300d4a9df4836a3ddbba1db1cbde2
ZippyDaFish <andrew.riker21@gmail.com>
2023-01-20 13:38:16 -0500

    Add forecast retrieval from api

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ async function getWeatherData(city, unit){
     console.log(weatherData);
     assignWeatherData(weatherData);
 }
-
 function assignWeatherData(weatherData){
     let tempFeelsLike = weatherData.main.feels_like;
     let temp = weatherData.main.temp;
@@ -12,4 +11,20 @@ function assignWeatherData(weatherData){
     let windSpeed = weatherData.wind.speed;
     let cloudCover = weatherData.weather[0].description;
 }
+
+async function getForecastData(city, unit){
+    const response = await fetch('http://api.openweathermap.org/data/2.5/forecast?q='+city+'&units='+unit+'&APPID=1bc93ee0ee0d4cdd8fd91c4abca090f2');
+    const forecastData = await response.json();
+    console.log(forecastData);
+    assignForecastData(forecastData.list);
+}
+function assignForecastData(forecastDataList){
+    let dataInDays = [];
+    dataInDays.push(forecastDataList[0])
+    for(i = 5; i < forecastDataList.length; i += 8){
+        dataInDays.push(forecastDataList[i]);
+    }
+    console.log(dataInDays);
+}
 getWeatherData('Lansing', 'metric');
+getForecastData('Lansing', 'metric');
